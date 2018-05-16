acc18826f9be454d2dc9774bcfeaa691bee99a07
Juan Pedro <salumvi@gmail.com>
2018-05-16 12:51:35 +0200

    Servicio firebase listo

--- src/app/services/info.service.ts
+++ src/app/services/info.service.ts
@@ -9,15 +9,36 @@ import { Injectable } from '@angular/core';
 export class InfoService {
 
   public infoPagina: any = {};
-  public cargando = false;
+  public infoEquipo: any = {};
+  public cargandoInfo = false;
+  public cargandoInfoNosotros = false;
+  
 
   constructor( public http: Http) {
-
-this.http.get('assets/data/info.pagina.json')
-        .subscribe(data => {
-          this.cargando = true;
-          this.infoPagina = data.json();
-        });
+  this.cargaInfo();
+this.cargaSobreNosotros();
 
    }
+
+public cargaInfo(){
+  this.http.get('assets/data/info.pagina.json')
+  .subscribe(data => {
+    this.cargandoInfo = true;
+    this.infoPagina = data.json();
+  });
+
+}
+
+public cargaSobreNosotros(){
+  this.http.get('https://paginaweb-d86a1.firebaseio.com/EQUIPO.json')
+  .subscribe(data => {
+    this.cargandoInfoNosotros = true;
+    console.log(data.json());
+    this.infoEquipo = data.json();
+  });
+
+}
+
+
+
 }
